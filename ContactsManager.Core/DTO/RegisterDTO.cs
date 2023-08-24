aa26b7f4f88c7b9e00327a491d3ad15f1aec2179
Mhmdkaraz <mohamedkaraz22@gmail.com>
2023-08-24 15:00:14 +0300

    Added RegisterDTO

---
+++ ContactsManager.Core/DTO/RegisterDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsManager.Core.DTO {
+    public class RegisterDTO {
+        [Required(ErrorMessage = "Name can't be blank")]
+        public string PersonName { get; set; }
+
+        [Required(ErrorMessage = "Email can't be blank")]
+        [EmailAddress(ErrorMessage = "Email should be in a proper email address format")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Phone can't be blank")]
+        [RegularExpression("^[0-9]*$",ErrorMessage = "Phone number should containg numbers only")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Password can't be blank")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password can't be blank")]
+        [DataType(DataType.Password)]
+        [Compare("Password",ErrorMessage = "{0} and {1} do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
